781d8af8aa833e97e80ed8e11a2f78113b17af15
devrenatorodrigues <devrenatorodrigues@gmail.com>
2024-03-18 14:18:43 -0300

    refactor: resolve imports

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     API_URL: "https://65f48404f54db27bc021e23e.mockapi.io/",
-    DOMAIN_ORIGIN: "https://65f48404f54db27bc021e23e.mockapi.io/"
+    DOMAIN_ORIGIN: "http://localhost:3000"
   }
 };
 
