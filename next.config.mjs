966686e3fbb0b7aa7a2f240527810bc3aa13611c
devrenatorodrigues <devrenatorodrigues@gmail.com>
2024-03-19 10:09:25 -0300

    refactor: heroes.json

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     API_URL: "https://65f48404f54db27bc021e23e.mockapi.io",
-    DOMAIN_ORIGIN: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000"
+    DOMAIN_ORIGIN: "https://spiderverse-pi.vercel.app",
   }
 };
 
