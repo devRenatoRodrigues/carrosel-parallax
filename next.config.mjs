7e3f8b650ef367b9a28ff8343ad1508ce1d76503
devrenatorodrigues <devrenatorodrigues@gmail.com>
2024-03-19 08:40:23 -0300

    refactor: changed the ORIGIN_DOMAIN

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API_URL: "https://65f48404f54db27bc021e23e.mockapi.io/",
+    API_URL: "https://65f48404f54db27bc021e23e.mockapi.io",
     DOMAIN_ORIGIN: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000"
   }
 };
